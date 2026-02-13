8be619f29063f2973d1475a17f258c4e7708633c
Brendan Duncan <brendan.duncan@unity3d.com>
2026-02-13 08:51:40 -0700

    updates

--- forge.config.js
+++ forge.config.js
@@ -4,6 +4,7 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
+    outDir: process.env.PACKAGE_OUT_DIR || 'out',
   },
   rebuildConfig: {},
   makers: [
